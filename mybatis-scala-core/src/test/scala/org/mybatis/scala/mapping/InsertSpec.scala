35b1e40e6a0f34d56e2af1f1a4578b19c51a8bc5
Eyal Farago <eyal.farago@actimize.com>
2014-11-05 09:54:13 +0200

    passingTests: fix the test (back to original state?) to match scalatest 2.2.2 matchers.

--- mybatis-scala-core/src/test/scala/org/mybatis/scala/mapping/InsertSpec.scala
+++ mybatis-scala-core/src/test/scala/org/mybatis/scala/mapping/InsertSpec.scala
@@ -1,15 +1,15 @@
 package org.mybatis.scala.mapping
 
-import org.scalatest._
-import org.mybatis.scala.infrastructure.UserRepository
 import org.mybatis.scala.DatabaseSupport
 import org.mybatis.scala.domain.User
-import org.scalatest.matchers.{ShouldMatchers, ClassicMatchers}
+import org.mybatis.scala.infrastructure.UserRepository
+import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
 
 /**
  * The specification for [[Insert]].
  */
-class InsertSpec extends FlatSpec with ClassicMatchers with ShouldMatchers with DatabaseSupport {
+class InsertSpec extends FlatSpec with Matchers with DatabaseSupport {
   "A Insert" should "insert User into user table" in {
     withReadOnly(Database.default) { implicit session =>
       val expected = User(0, "test", "example@example.com")
--- mybatis-scala-core/src/test/scala/org/mybatis/scala/mapping/StatementSpec.scala
+++ mybatis-scala-core/src/test/scala/org/mybatis/scala/mapping/StatementSpec.scala
@@ -1,14 +1,14 @@
 package org.mybatis.scala.mapping
 
-import org.scalatest._
 import org.mybatis.scala.config.ConfigurationException
-import org.scalatest.matchers.{ShouldMatchers, ClassicMatchers, Matchers}
+import org.scalatest._
+
 import scala.util.control.NonFatal
 
 /**
  * The specification for [[Statement]].
  */
-class StatementSpec extends FlatSpec with ClassicMatchers with ShouldMatchers {
+class StatementSpec extends FlatSpec with Matchers {
   val simpleStatement = new Statement {
   override def parameterTypeClass: Class[_] = classOf[Unit]
     override def xsql: XSQL = <xsql>SELECT 1</xsql>
